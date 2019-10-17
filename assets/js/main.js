10cc7bd97bf826e6c9ec6cc63d0b777442fa5cf7
MaksimBorovik <borovik.maksim@gmail.com>
2019-10-18 01:21:45 +0300

    code optimization

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   $('.review__slider').slick({
+    // autoplay: true,
     arrows: false,
     dots: true,
+    swipeToSlide: true,
     slidesToShow: 3,
     slidesToScroll: 1
   });
@@ -16,4 +18,8 @@ $('.fa-times-circle').click(function () {
   $('.main__chat').removeClass('active');
   $('.main__chat-marker').addClass('active')
   $('.main__chat-window-dialog-response p').removeClass('active');
+})
+
+$('.navbar-toggler').click(function () {
+  $('.header__menu-navbar-toggler-icon').toggleClass('active');
 })
